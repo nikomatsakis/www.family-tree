f89eef19bf2884fb94928ea8cf280512fbe3504a
Niko Matsakis <niko@alum.mit.edu>
2024-08-25 19:07:19 -0400

    more updates

--- app/components/person-link.js
+++ app/components/person-link.js
@@ -1,5 +1,7 @@
 import Component from '@glimmer/component';
 
 export default class PersonLinkComponent extends Component {
-
+    get isPagePerson() {
+        return this.args.person == this.args.pagePerson;
+    }
 }
