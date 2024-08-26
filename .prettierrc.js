d4773a7588333ba9c9b8a399c8ce6de6a521840c
Yehuda Katz <wycats@gmail.com>
2024-08-26 16:36:21 -0700

    Configure prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+// .prettierrc.js
+module.exports = {
+  // ...
+  plugins: ['prettier-plugin-ember-template-tag'],
+  overrides: [
+    {
+      files: '*.{js,ts,gjs,gts}',
+      options: {
+        singleQuote: true,
+      },
+    },
+    // ...
+  ],
+};
